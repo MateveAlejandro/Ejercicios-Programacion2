07fa4a393fe2741b30b47ea5c42aa7c5f23f7847
MateveAlejandro <129460203+MateveAlejandro@users.noreply.github.com>
2023-04-27 21:30:58 -0300

    Add files via upload

---
+++ Clase 4/JS/funciones.js
@@ -0,0 +1,10 @@
+function mifuncion () {
+    const jugador ={
+        nombre: "Barrios",
+        Edad: 24,
+        Activo: "Tigre",
+        Apodo: "Perrito",
+        Saludar: function(){return "Hola"},
+    };
+    console.log(jugador.Saludar()); //Puede ser también (jugador.apodo). fetch leer  promise leer xmlhttprequest
+}
